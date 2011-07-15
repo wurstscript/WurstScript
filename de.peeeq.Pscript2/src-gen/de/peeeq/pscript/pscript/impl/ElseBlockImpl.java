568277cc89f2e12b89a91f3e47bcde6ed3c90dc6
Peter <Peter@Peter-Laptop>
2011-07-15 14:46:53 +0200

    ...

---
+++ de.peeeq.Pscript2/src-gen/de/peeeq/pscript/pscript/impl/ElseBlockImpl.java
@@ -0,0 +1,48 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+
+ */
+package de.peeeq.pscript.pscript.impl;
+
+import de.peeeq.pscript.pscript.ElseBlock;
+import de.peeeq.pscript.pscript.PscriptPackage;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Else Block</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class ElseBlockImpl extends MinimalEObjectImpl.Container implements ElseBlock
+{
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected ElseBlockImpl()
+  {
+    super();
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  protected EClass eStaticClass()
+  {
+    return PscriptPackage.Literals.ELSE_BLOCK;
+  }
+
+} //ElseBlockImpl
