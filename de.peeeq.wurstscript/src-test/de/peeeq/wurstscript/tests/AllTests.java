0d4d838d7149f9795486dc64b97bc699bbb1f7fc
Peter.peq <devnull@localhost>
2011-11-29 18:53:11 +0100

    unit tests

---
+++ de.peeeq.wurstscript/src-test/de/peeeq/wurstscript/tests/AllTests.java
@@ -0,0 +1,11 @@
+package de.peeeq.wurstscript.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ SimpleStatementTests.class })
+public class AllTests {
+
+}
