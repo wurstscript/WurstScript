4ae8e0db8d4150c4f446cbfd7433aaedc4079d3e
Peter.peq <devnull@localhost>
2012-01-29 20:06:35 +0100

    fixed bug with new-exprs

--- de.peeeq.wurstscript/src/de/peeeq/wurstscript/attributes/AttrExprExpectedType.java
+++ de.peeeq.wurstscript/src/de/peeeq/wurstscript/attributes/AttrExprExpectedType.java
@@ -1,14 +1,19 @@
 package de.peeeq.wurstscript.attributes;
 
+import java.util.List;
+import java.util.Map;
+
 import de.peeeq.wurstscript.ast.Arguments;
 import de.peeeq.wurstscript.ast.AstElement;
 import de.peeeq.wurstscript.ast.Expr;
 import de.peeeq.wurstscript.ast.ExprBinary;
 import de.peeeq.wurstscript.ast.ExprFunctionCall;
 import de.peeeq.wurstscript.ast.ExprNewObject;
+import de.peeeq.wurstscript.ast.FunctionCall;
 import de.peeeq.wurstscript.ast.FunctionImplementation;
 import de.peeeq.wurstscript.ast.StmtReturn;
 import de.peeeq.wurstscript.ast.StmtSet;
+import de.peeeq.wurstscript.ast.TypeParamDef;
 import de.peeeq.wurstscript.ast.VarDef;
 import de.peeeq.wurstscript.ast.WParameters;
 import de.peeeq.wurstscript.types.PScriptTypeUnknown;
@@ -21,18 +26,23 @@ public class AttrExprExpectedType {
 			AstElement parent = expr.getParent();
 			if (parent instanceof Arguments) {
 				Arguments args = (Arguments) parent;
-				WParameters params = null;
-				if (args.getParent() instanceof ExprFunctionCall) {
-					ExprFunctionCall efc = (ExprFunctionCall) args.getParent();
+				WParameters params;
+				Map<TypeParamDef, PscriptType> typeParamBindings;
+				if (args.getParent() instanceof FunctionCall) {
+					FunctionCall efc = (FunctionCall) args.getParent();
 					params = efc.attrFuncDef().getParameters();
+					typeParamBindings = efc.attrTypeParameterBindings();
 				} else if (args.getParent() instanceof ExprNewObject) {
 					ExprNewObject eno = (ExprNewObject) args.getParent();
 					params = eno.attrConstructorDef().getParameters();
+					typeParamBindings = eno.attrTypeParameterBindings();
+				} else {
+					throw new CompileError(expr.getSource(), "could not calculate expected type");
 				}
 				for (int i = 0; i < args.size(); i++) {
 					if (args.get(i) == expr) {
 						PscriptType paramType = params.get(i).attrTyp();
-						return paramType;
+						return paramType.setTypeArgs(typeParamBindings);
 					}
 				}
 				throw new CompileError(expr.getSource(), "could not find expr in parent");
