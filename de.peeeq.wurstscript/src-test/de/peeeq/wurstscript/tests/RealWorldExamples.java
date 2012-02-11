f2ed11521554f662afcd9c808e5424527d6b2287
killuag <killuag@trash-mail.com>
2012-02-12 00:30:24 +0100

    tuples are so bugged

--- de.peeeq.wurstscript/src-test/de/peeeq/wurstscript/tests/RealWorldExamples.java
+++ de.peeeq.wurstscript/src-test/de/peeeq/wurstscript/tests/RealWorldExamples.java
@@ -67,6 +67,11 @@ public class RealWorldExamples extends PscriptTest {
 	}
 	
 	@Test
+	public void frottyTupleBug() throws IOException {
+		super.testAssertOkFileWithStdLib(new File(TEST_DIR + "tupleBug.wurst"), false);
+	}
+	
+	@Test
 	public void test_stdlib() throws IOException {
 		List<File> inputs = Lists.newLinkedList();
 //		settings.put("lib", "./wurstscript/lib/");
