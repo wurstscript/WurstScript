59a50251fee5d8fb2f45cbe2252006acf69e76ae
peq <Peter.peq@googlemail.com>
2012-07-21 21:32:42 +0200

    new: partition data structure

---
+++ de.peeeq.wurstscript/src/de/peeeq/datastructures/Partitions.java
@@ -0,0 +1,85 @@
+package de.peeeq.datastructures;
+
+import java.util.Collection;
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
+import de.peeeq.wurstscript.ast.ClassDef;
+
+public class Partitions<T> {
+	
+	private class Partition {
+		private Partition rep;
+		private T item;
+		
+		public Partition(T item) {
+			rep = this;
+			this.item = item;
+		}
+
+		public void unionWith(Partition other) {
+			rep = other.getRep();			
+		}
+
+		private Partition getRep() {
+			if (rep == this) {
+				return this;
+			} else {
+				rep = rep.getRep(); // path compression
+				return rep;
+			}
+		}
+	}
+	
+	private Map<T, Partition> partitions = Maps.newHashMap();
+	
+	public Partitions() {
+	}
+	
+	public Partitions(Collection<T> items) {
+		for (T t : items) {
+			partitions.put(t, new Partition(t));
+		}
+	}
+	
+	/**
+	 * unions the partition of a and the partition of b 
+	 */
+	public void union(T a, T b) {
+		getPartition(a).unionWith(getPartition(b));
+	}
+
+	
+	
+	/**
+	 * add a new partition
+	 */
+	public void add(T t) {
+		if (getPartition(t) == null) {
+			partitions.put(t, new Partition(t));
+		}
+	}
+	
+	/**
+	 * get the representing item for the partition containing t 
+	 */
+	public T getRep(T t) {
+		return getPartition(t).getRep().item;
+	}
+	
+	
+	
+	private Partition getPartition(T b) {
+		Partition p = partitions.get(b);
+		if (p == null) {
+			p = new Partition(b);
+			partitions.put(b, p);
+		}
+		return p;
+	}
+
+	public boolean contains(T t) {
+		return partitions.containsKey(t);
+	}
+}
