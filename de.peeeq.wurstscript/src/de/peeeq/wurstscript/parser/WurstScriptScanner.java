1add3f22b4d6e45abaa1b3b96995ba8795d76727
Peter Zeller <Peter.peq@googlemail.com>
2012-08-08 23:18:28 +0200

    fixed parse error when newline was missing at end of file

--- de.peeeq.wurstscript/src/de/peeeq/wurstscript/parser/WurstScriptScanner.java
+++ de.peeeq.wurstscript/src/de/peeeq/wurstscript/parser/WurstScriptScanner.java
@@ -6,6 +6,9 @@ import java_cup.runtime.Symbol;
 
 public class WurstScriptScanner extends WurstScriptScannerIntern {
 
+	private int indent = 0;
+	private Symbol eof = null;
+
 	public WurstScriptScanner(InputStream in) {
 		super(in);
 	}
@@ -15,7 +18,18 @@ public class WurstScriptScanner extends WurstScriptScannerIntern {
 	
 	@Override
 	public java_cup.runtime.Symbol next_token() throws java.io.IOException {
+		if (eof != null) {
+			// finish unindents...
+			if (indent > 0) {
+				indent--;
+				return new Symbol(TokenType.UNINDENT, eof.left, eof.right);
+			} else {
+				return new Symbol(TokenType.EOF, eof.left, eof.right);
+			}
+		}
+		
 		Symbol token;
+		
 		if (!returnStack.isEmpty()) {
 			token = returnStack.pop();
 		} else {
@@ -24,6 +38,21 @@ public class WurstScriptScanner extends WurstScriptScannerIntern {
 			} while (token == null);
 
 		}
+		
+		if (token.sym == TokenType.INDENT) {
+			indent++;
+		} else if (token.sym == TokenType.UNINDENT) {
+			indent--;
+		} else if (token.sym == TokenType.EOF) {
+			if (indent > 0) {
+				// when we encounter an end of file and still have indentation then we add
+				// artificial newlines and unindents
+				eof = token;
+				return new Symbol(TokenType.NL, eof.left, eof.right);
+			}
+		}
+		
+		
 		return token;
 	}
 
