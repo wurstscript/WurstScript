90acd6907c5cc35e10ebf6b045deba2de8f47bc1
Peter Zeller <Peter.peq@googlemail.com>
2012-12-15 16:22:13 +0100

    fixed name compressor

--- de.peeeq.wurstscript/src/de/peeeq/wurstscript/translation/imoptimizer/ImCompressor.java
+++ de.peeeq.wurstscript/src/de/peeeq/wurstscript/translation/imoptimizer/ImCompressor.java
@@ -1,15 +1,8 @@
 package de.peeeq.wurstscript.translation.imoptimizer;
 
-import java.util.HashMap;
-
-import com.google.common.collect.Maps;
-
 import de.peeeq.wurstscript.jassIm.ImFunction;
-import de.peeeq.wurstscript.jassIm.ImFunctions;
 import de.peeeq.wurstscript.jassIm.ImProg;
 import de.peeeq.wurstscript.jassIm.ImVar;
-import de.peeeq.wurstscript.jassIm.ImVarRead;
-import de.peeeq.wurstscript.jassIm.ImVarWrite;
 import de.peeeq.wurstscript.jassoptimizer.NameGenerator;
 import de.peeeq.wurstscript.translation.imtranslation.ImTranslator;
 
@@ -18,13 +11,11 @@ public class ImCompressor {
 	private ImTranslator translator;
 	private ImProg prog;
 	private NameGenerator ng;
-	final HashMap<String, String> replacements;
 	
 	public ImCompressor(ImTranslator translator) {
 		this.translator = translator;
 		this.prog = translator.getImProg();
 		ng = new NameGenerator();
-		replacements = Maps.newHashMap();
 	}
 	
 	public void compressNames() {
@@ -37,38 +28,23 @@ public class ImCompressor {
 	
 	public void compressGlobals() {
 		for ( final ImVar global : prog.getGlobals() ) {
+			if (global.getIsBJ()) {
+				// no not rename bj constants
+				continue;
+			}
 			String replacement = ng.getUniqueToken();
 			global.setName(replacement);
-//			for ( ImVarWrite globalWrite : global.attrWrites()) {
-//				globalWrite.getLeft().setName(replacement);
-//			}
-//			for ( ImVarRead globalRead : global.attrReads()) {
-//				globalRead.getVar().setName(replacement);
-//			}
-			
 		}
 	}
 	
 	public void compressFunctions() {
 		for( ImFunction func : prog.getFunctions() ) {
-			if (! func.isNative() ) {
-				String rname = ng.getUniqueToken();
-				replacements.put(func.getName(), rname);
+			if (func.isNative() || func.isBj() || func.isCompiletime()) {
+				// do not rename builtin an bj functions
+				continue;
 			}
-		}
-		
-//		for( ImFunction func : prog.getFunctions() ) {
-//			for (ImFunction called : translator.getCalledFunctions().get(func)) {
-//				if (replacements.containsKey(called.getName()))
-//					called.setName(replacements.get(called.getName()));
-//			}
-//		}
-		
-		for( ImFunction func : prog.getFunctions() ) {
-			if (! func.isNative() ) {
-				if (replacements.containsKey(func.getName()))
-					func.setName(replacements.get(func.getName()));
-			}
+			String rname = ng.getUniqueToken();
+			func.setName(rname);
 		}
 		
 	}
