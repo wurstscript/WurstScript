4badf2ddafcac28ab1f9bf3818f0542c1f449077
Peter Zeller <Peter.peq@googlemail.com>
2013-01-05 13:16:38 +0100

    I hope this fixes the bug in the error reporter
    now only parsed files are sent ...

--- de.peeeq.wurstscript/src-test/de/peeeq/wurstscript/utils/ErrorReportingTest.java
+++ de.peeeq.wurstscript/src-test/de/peeeq/wurstscript/utils/ErrorReportingTest.java
@@ -2,7 +2,13 @@ package de.peeeq.wurstscript.utils;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.junit.Test;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
 
 import de.peeeq.wurstscript.ErrorReporting;
 
@@ -13,5 +19,29 @@ public class ErrorReportingTest {
 		boolean result = ErrorReporting.sendErrorReport(new Error("bla"), "source");
 		assertEquals(true, result);
 	}
+	
+	@Test
+	public void testBigSource() throws IOException {
+		StringBuilder source = new StringBuilder();
+		appendFileContentsOf(new File("../Wurstpack/wurstscript/lib"), source);
+		
+		boolean result = ErrorReporting.sendErrorReport(new Error("bla"), source.toString());
+		assertEquals(true, result);
+	}
+
+	private void appendFileContentsOf(File f, StringBuilder sb) throws IOException {
+		assertTrue("file  does not exist:" + f, f.exists());
+		if (f.isDirectory()) {
+			for (File child : f.listFiles()) {
+				appendFileContentsOf(child, sb);
+			}
+		} else {
+			if (f.getName().endsWith(".wurst")) {
+				sb.append("\n\n//################################\n\n");
+				sb.append(Files.toString(f, Charsets.UTF_8));
+			}
+		}
+	}
+
 
 }
