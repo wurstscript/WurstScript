d62b411a3a34c8460f13ee538ace022aa19e13c9
Peter Zeller <Peter.peq@googlemail.com>
2013-12-21 23:04:51 +0100

    version 1.1.0.9

--- de.peeeq.wurstscript/src/de/peeeq/wurstio/gui/About.java
+++ de.peeeq.wurstscript/src/de/peeeq/wurstio/gui/About.java
@@ -27,7 +27,7 @@ import de.peeeq.wurstscript.CompileTimeInfo;
 public class About extends javax.swing.JDialog {
     final URI uri;
     final static String homepage = "http://peq.github.com/WurstScript/";
-    public final static String version = "1.1.0.8." + CompileTimeInfo.revision;
+    public final static String version = "1.1.0.9." + CompileTimeInfo.revision;
     final static String authors = "peq & Frotty";
 
     /** Creates new form About */
