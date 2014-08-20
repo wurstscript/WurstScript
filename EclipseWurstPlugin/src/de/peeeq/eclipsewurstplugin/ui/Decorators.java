3f32adebca5fd03df7e0af6aee910b379f0a8c88
Peter Zeller <Peter.peq@googlemail.com>
2014-08-21 00:12:09 +0200

    added listener to decorations

--- EclipseWurstPlugin/src/de/peeeq/eclipsewurstplugin/ui/Decorators.java
+++ EclipseWurstPlugin/src/de/peeeq/eclipsewurstplugin/ui/Decorators.java
@@ -1,18 +1,51 @@
 package de.peeeq.eclipsewurstplugin.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.IDecoration;
+import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.jface.viewers.ILightweightLabelDecorator;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.LabelProviderChangedEvent;
+import org.eclipse.swt.widgets.Display;
+
+import com.google.common.base.Preconditions;
 
 import de.peeeq.eclipsewurstplugin.editor.outline.Icons;
 
-public class Decorators extends LabelProvider implements ILightweightLabelDecorator  {
+public class Decorators extends LabelProvider implements ILightweightLabelDecorator, IResourceChangeListener {
 
+	private List<ILabelProviderListener> listeners = new ArrayList<ILabelProviderListener>();
+	
+	
+	public Decorators() {
+		ResourcesPlugin.getWorkspace().addResourceChangeListener(this);
+	}
+	
+	@Override
+	public void dispose() {
+		ResourcesPlugin.getWorkspace().removeResourceChangeListener(this);
+	}
+	
+	@Override
+	public void addListener(@Nullable ILabelProviderListener listener) {
+		if (listener == null) return;
+		listeners.add(listener);
+	}
+	
+	@Override
+	public void removeListener(@Nullable ILabelProviderListener listener) {
+		listeners.remove(listener);
+	}
 
 	@Override
 	public void decorate(@Nullable Object element, @Nullable IDecoration decoration) {
@@ -45,6 +78,25 @@ public class Decorators extends LabelProvider implements ILightweightLabelDecora
 			} catch (CoreException e) {
 				e.printStackTrace();
 			}
+		}
+	}
+
+	@Override
+	public void resourceChanged(@Nullable IResourceChangeEvent event) {
+		Preconditions.checkNotNull(event);
+		if (event.getType() == IResourceChangeEvent.POST_CHANGE) {
+			Display.getDefault().asyncExec(new Runnable() {
+				@Override
+				public void run() {
+					fireLabelChangedEvent();
+				}
+			});
+		}
+	}
+
+	private void fireLabelChangedEvent() {
+		for (ILabelProviderListener listener : listeners) {
+			listener.labelProviderChanged(new LabelProviderChangedEvent(this));
 		}
 	}
 
