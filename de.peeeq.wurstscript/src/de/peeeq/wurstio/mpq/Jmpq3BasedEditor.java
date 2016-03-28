0b82a160ee245b246be3fc9c85a11c514e4720f3
Frotty <frotty@sunayama.de>
2016-03-28 05:09:43 +0200

    Update to newest jmpq3

--- de.peeeq.wurstscript/src/de/peeeq/wurstio/mpq/Jmpq3BasedEditor.java
+++ de.peeeq.wurstscript/src/de/peeeq/wurstio/mpq/Jmpq3BasedEditor.java
@@ -6,65 +6,61 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 
+import systems.crigges.jmpq3.JMpqEditor;
+import systems.crigges.jmpq3.JMpqException;
+
 import com.google.common.base.Preconditions;
-
-import de.peeeq.jmpq3.JMpqEditor;
-import de.peeeq.jmpq3.JMpqException;
-
-
 
 class Jmpq3BasedEditor implements MpqEditor {
 
-	private JMpqEditor editor;
+    private JMpqEditor editor;
 
-	private JMpqEditor getEditor() {
-		if (editor == null) {
-			throw new RuntimeException("editor already closed");
-		}
-		return editor;
-	}
-	public Jmpq3BasedEditor(File mpqArchive) throws Exception {
-		Preconditions.checkNotNull(mpqArchive);
-		if (!mpqArchive.exists()) {
-			throw new FileNotFoundException("not found: " + mpqArchive);
-		}
-		this.editor = new JMpqEditor(mpqArchive);
-	}
+    private JMpqEditor getEditor() {
+        if (editor == null) {
+            throw new RuntimeException("editor already closed");
+        }
+        return editor;
+    }
 
-	@Override
-	public void insertFile(String filenameInMpq, byte[] contents) throws Exception {
-		File temp = File.createTempFile("peq", "wurst");
-		FileOutputStream fos = new FileOutputStream(temp);
-		fos.write(contents);
-		fos.close();
-		getEditor().insertFile(filenameInMpq, temp, false);
-	}
-	
-	
-	@Override
-	public byte[] extractFile(String fileToExtract)	throws Exception {
-		File temp = File.createTempFile("peq", "wurst");
-		getEditor().extractFile(fileToExtract, temp);
-		return Files.readAllBytes(temp.toPath());
-	}
+    public Jmpq3BasedEditor(File mpqArchive) throws Exception {
+        Preconditions.checkNotNull(mpqArchive);
+        if (!mpqArchive.exists()) {
+            throw new FileNotFoundException("not found: " + mpqArchive);
+        }
+        this.editor = new JMpqEditor(mpqArchive);
+    }
 
+    @Override
+    public void insertFile(String filenameInMpq, byte[] contents) throws Exception {
+        File temp = File.createTempFile("peq", "wurst");
+        FileOutputStream fos = new FileOutputStream(temp);
+        fos.write(contents);
+        fos.close();
+        getEditor().insertFile(filenameInMpq, temp, false);
+    }
 
-	@Override
-	public void deleteFile(String filenameInMpq) throws Exception {
-		getEditor().deleteFile(filenameInMpq);
-	}
+    @Override
+    public byte[] extractFile(String fileToExtract) throws Exception {
+        File temp = File.createTempFile("peq", "wurst");
+        getEditor().extractFile(fileToExtract, temp);
+        return Files.readAllBytes(temp.toPath());
+    }
 
-	@Override
-	public void close() throws IOException {
-		if (editor != null) {
-			try {
-				editor.close();
-			} catch (JMpqException e) {
-				throw new IOException(e);
-			}
-			editor = null;
-		}
-	}
+    @Override
+    public void deleteFile(String filenameInMpq) throws Exception {
+        getEditor().deleteFile(filenameInMpq);
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (editor != null) {
+            try {
+                editor.close();
+            } catch (JMpqException e) {
+                throw new IOException(e);
+            }
+            editor = null;
+        }
+    }
 
 }
-
