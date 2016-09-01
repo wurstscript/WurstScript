3570500c8ae55c9dc3f369d45faba3b007be68c4
Peter Zeller <Peter.peq@googlemail.com>
2016-09-01 21:24:45 +0200

    missed file in last commit

---
+++ de.peeeq.wurstscript/src-test/tests/wurstscript/tests/StdLib.java
@@ -0,0 +1,76 @@
+package tests.wurstscript.tests;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import de.peeeq.wurstscript.utils.Utils;
+
+/**
+ * Helper class to download the standard library, which is required by some test
+ * cases
+ */
+public class StdLib {
+
+	/** the repo to download */
+	private final static String gitRepo = "https://github.com/peq/wurstStdlib.git";
+	
+	/** version to use for the tests */
+	private final static String version = "7ea2806aa56320bf00d6a9fa3615adfca851d66c";
+
+	/** flag so that initialization in only done once */
+	private static boolean isInitialized = false; 
+	
+	private static File tempFolder = new File("./temp");
+	private static File stdLibFolder = new File(tempFolder, "wurstStdlib");
+	
+	@Test
+	public void download() {
+		downloadStandardlib();
+	}
+	
+	public synchronized static void downloadStandardlib() {
+		if (isInitialized) {
+			return;
+		}
+		try {
+			
+
+			if (!stdLibFolder.exists()) {
+				Utils.exec(tempFolder, "git", "clone", gitRepo, "wurstStdlib");
+			}
+			
+			String revision = Utils.exec(stdLibFolder, "git", "rev-parse", "HEAD");
+			System.out.println("revision = " + revision);
+			if (!revision.equals(version)) {
+				Utils.exec(stdLibFolder, "git", "checkout", "master");
+				Utils.exec(stdLibFolder, "git", "pull");
+				Utils.exec(stdLibFolder, "git", "checkout", version);
+			}
+			
+			// reset all possible changes
+			Utils.exec(stdLibFolder, "git", "clean", "-fdx");
+			Utils.exec(stdLibFolder, "git", "checkout", ".");
+			Utils.exec(stdLibFolder, "git", "checkout", version);
+			
+			isInitialized = true;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static String getLib() {
+		downloadStandardlib();
+		return stdLibFolder.getPath();
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
