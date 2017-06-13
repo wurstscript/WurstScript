5250ad1926e9d7812200135a0a3ae68b7b620a09
Peter Zeller <Peter.peq@googlemail.com>
2017-06-13 20:10:56 +0200

    remove parseq
    new version at
    https://github.com/peterzeller/abstractsyntaxgen

--- parseq/src/de/peeeq/parseq/asts/ast/AstEntityDefinition.java
+++
@@ -1,5 +0,0 @@
-package de.peeeq.parseq.asts.ast;
-
-public interface AstEntityDefinition {
-	String getName();
-}
--- parseq/src/de/peeeq/parseq/grammars/ast/RepeatType.java
+++
@@ -1,5 +0,0 @@
-package de.peeeq.parseq.grammars.ast;
-
-public enum RepeatType {
-	ZERO_OR_ONCE, ARBITRARY, AT_LEAST_ONCE 
-}
