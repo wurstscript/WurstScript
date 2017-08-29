43e19ad7f42046d0c0b14f332f229321748a4064
Peter Zeller <Peter.peq@googlemail.com>
2017-08-29 22:30:13 +0200

    add unique tag to log files

--- de.peeeq.wurstscript/src/main/java/de/peeeq/wurstio/Main.java
+++ de.peeeq.wurstscript/src/main/java/de/peeeq/wurstio/Main.java
@@ -424,7 +424,7 @@ public class Main {
             File folder = new File(tempDir, folderName);
             if (folder.exists() || folder.mkdirs()) {
                 System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$-6s %5$s%6$s%n");
-                Handler handler = new FileHandler(folder.getAbsolutePath() + "/wurst%g.log", Integer.MAX_VALUE, 5);
+                Handler handler = new FileHandler(folder.getAbsolutePath() + "/wurst%g-%u.log", Integer.MAX_VALUE, 5);
                 handler.setFormatter(new SimpleFormatter());
                 WLogger.setHandler(handler);
                 WLogger.setLevel(Level.INFO);
