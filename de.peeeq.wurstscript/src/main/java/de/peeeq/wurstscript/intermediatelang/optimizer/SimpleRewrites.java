87433bc5b9bb1d3d0acfbaa1691f5a668aaec499
Frotty <frotty@sunayama.de>
2018-06-13 00:54:28 +0200

    fix local opts exp runtime

--- de.peeeq.wurstscript/src/main/java/de/peeeq/wurstscript/intermediatelang/optimizer/SimpleRewrites.java
+++ de.peeeq.wurstscript/src/main/java/de/peeeq/wurstscript/intermediatelang/optimizer/SimpleRewrites.java
@@ -91,7 +91,6 @@ public class SimpleRewrites implements OptimizerPass {
         }
         if (elem instanceof ImOperatorCall) {
             ImOperatorCall opc = (ImOperatorCall) elem;
-            optimizeElement(opc.getArguments());
             optimizeOpCall(opc);
         } else if (elem instanceof ImIf) {
             ImIf imIf = (ImIf) elem;
