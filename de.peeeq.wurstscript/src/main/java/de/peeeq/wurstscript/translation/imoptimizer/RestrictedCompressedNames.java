56aa8ebb2e87debb751a83884d0bb076b544083e
Frotty <frotty@sunayama.de>
2018-07-21 14:59:51 +0200

    prevent functions named set in generated code

--- de.peeeq.wurstscript/src/main/java/de/peeeq/wurstscript/translation/imoptimizer/RestrictedCompressedNames.java
+++ de.peeeq.wurstscript/src/main/java/de/peeeq/wurstscript/translation/imoptimizer/RestrictedCompressedNames.java
@@ -3,7 +3,8 @@ package de.peeeq.wurstscript.translation.imoptimizer;
 public class RestrictedCompressedNames {
     static String names[] = {"div", "val", "var", "mod", "use", "new", "for", "in", "it", "to",
             "Sin", "Cos", "Tan", "Pow", "I2R", "R2I", "I2S", "R2S", "S2I", "S2R",
-            "And", "Or", "Not", "or", "and", "not", "if", "set"};
+            "And", "Or", "Not", "or", "and", "not", "if", "set", "loop", "endif",
+            "endfunction", "endloop", "globals", "endglobals", "local", "call"};
 
     public static boolean contains(String s) {
         for (String name : names) {
