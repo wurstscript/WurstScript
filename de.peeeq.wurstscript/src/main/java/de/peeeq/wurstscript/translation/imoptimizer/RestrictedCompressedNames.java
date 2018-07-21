db542b50ab3e5be41b31aa77384700ba52236c03
Frotty <frotty@sunayama.de>
2018-07-21 16:04:23 +0200

    also add 'debug' to restricted names, fixes #695

--- de.peeeq.wurstscript/src/main/java/de/peeeq/wurstscript/translation/imoptimizer/RestrictedCompressedNames.java
+++ de.peeeq.wurstscript/src/main/java/de/peeeq/wurstscript/translation/imoptimizer/RestrictedCompressedNames.java
@@ -4,7 +4,7 @@ public class RestrictedCompressedNames {
     static String names[] = {"div", "val", "var", "mod", "use", "new", "for", "in", "it", "to",
             "Sin", "Cos", "Tan", "Pow", "I2R", "R2I", "I2S", "R2S", "S2I", "S2R",
             "And", "Or", "Not", "or", "and", "not", "if", "set", "loop", "endif",
-            "endfunction", "endloop", "globals", "endglobals", "local", "call"};
+            "endfunction", "endloop", "globals", "endglobals", "local", "call", "debug"};
 
     public static boolean contains(String s) {
         for (String name : names) {
