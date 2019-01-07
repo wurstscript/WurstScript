297797bd85b9a7562c15f5abb96ef977be786ace
Peter Zeller <Peter.peq@googlemail.com>
2019-01-07 21:25:30 +0100

    fixed interpreter bug that somehow does not exist on master

--- de.peeeq.wurstscript/src/main/java/de/peeeq/wurstio/jassinterpreter/NativeJassFunction.java
+++ de.peeeq.wurstscript/src/main/java/de/peeeq/wurstio/jassinterpreter/NativeJassFunction.java
@@ -1,6 +1,7 @@
 package de.peeeq.wurstio.jassinterpreter;
 
 import de.peeeq.wurstscript.intermediatelang.ILconst;
+import de.peeeq.wurstscript.intermediatelang.ILconstNull;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -18,8 +19,13 @@ public class NativeJassFunction implements ExecutableJassFunction {
 
     @Override
     public ILconst execute(JassInterpreter jassInterpreter, ILconst[] arguments) {
-        System.out.println("execute " + this.provider + " . " + this.method.getName() + " " + Arrays.toString(arguments));
         try {
+            for (int i = 0; i < arguments.length; i++) {
+                if (arguments[i] instanceof ILconstNull
+                        && !method.getParameterTypes()[i].isAssignableFrom(arguments[i].getClass())) {
+                    arguments[i] = null;
+                }
+            }
             Object result = method.invoke(provider, (Object[]) arguments);
             return (ILconst) result;
         } catch (IllegalArgumentException | IllegalAccessException e) {
