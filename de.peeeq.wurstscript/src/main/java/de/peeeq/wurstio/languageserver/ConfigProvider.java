dfd8f5926600bcf2d44daa0009a05499a419dadf
Peter Zeller <Peter.peq@googlemail.com>
2019-06-21 22:08:16 +0200

    fixed merge syntax error

--- de.peeeq.wurstscript/src/main/java/de/peeeq/wurstio/languageserver/ConfigProvider.java
+++ de.peeeq.wurstscript/src/main/java/de/peeeq/wurstio/languageserver/ConfigProvider.java
@@ -57,11 +57,3 @@ public class ConfigProvider {
     }
 }
 
-    /**
-     * The path where to put maps before running them
-     */
-    public String getMapDocumentPath() {
-        return getConfig("mapDocumentPath", null);
-    }
-}
-
