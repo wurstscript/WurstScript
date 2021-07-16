a9a39323a9c9dac1ff1143fdc592e39af54c826f
Frotty <Frotty@users.noreply.github.com>
2021-07-16 02:13:00 +0200

    add 'alias' as restricted name, update pjass, fixes #1013

--- de.peeeq.wurstscript/src/main/java/de/peeeq/wurstscript/translation/imoptimizer/RestrictedCompressedNames.java
+++ de.peeeq.wurstscript/src/main/java/de/peeeq/wurstscript/translation/imoptimizer/RestrictedCompressedNames.java
@@ -5,7 +5,7 @@ public class RestrictedCompressedNames {
             "Sin", "Cos", "Tan", "Pow", "I2R", "R2I", "I2S", "R2S", "S2I", "S2R",
             "And", "Or", "Not", "or", "and", "not", "if", "set", "loop", "endif",
             "endfunction", "endloop", "globals", "endglobals", "local", "call", "debug",
-            "main", "config"};
+            "main", "config", "alias"};
 
     public static boolean contains(String s) {
         for (String name : names) {
