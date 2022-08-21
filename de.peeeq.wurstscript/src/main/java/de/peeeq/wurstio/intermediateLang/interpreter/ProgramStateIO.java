68e0abc2f2c1913aabad4cc15e3623778db9e022
Frotty <Frotty@users.noreply.github.com>
2022-08-22 01:14:11 +0200

    Refactor Object Mod Handling to use wc3libs (#1066)
    Fixes format version issues with Patch 1.33

--- de.peeeq.wurstscript/src/main/java/de/peeeq/wurstio/intermediateLang/interpreter/ProgramStateIO.java
+++ de.peeeq.wurstscript/src/main/java/de/peeeq/wurstio/intermediateLang/interpreter/ProgramStateIO.java
@@ -9,14 +9,20 @@ import de.peeeq.wurstscript.gui.WurstGui;
 import de.peeeq.wurstscript.intermediatelang.interpreter.ProgramState;
 import de.peeeq.wurstscript.jassIm.ImProg;
 import de.peeeq.wurstscript.jassIm.ImStmt;
+import de.peeeq.wurstscript.utils.Utils;
+import net.moonlightflower.wc3libs.bin.ObjMod;
+import net.moonlightflower.wc3libs.bin.Wc3BinInputStream;
+import net.moonlightflower.wc3libs.bin.Wc3BinOutputStream;
+import net.moonlightflower.wc3libs.bin.app.objMod.*;
+import net.moonlightflower.wc3libs.dataTypes.app.War3Int;
+import net.moonlightflower.wc3libs.dataTypes.app.War3String;
 import net.moonlightflower.wc3libs.txt.WTS;
 import org.eclipse.jdt.annotation.Nullable;
+import org.jetbrains.annotations.NotNull;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintStream;
+import java.io.*;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -24,10 +30,11 @@ public class ProgramStateIO extends ProgramState {
 
     private static final int GENERATED_BY_WURST = 42;
     private @Nullable ImStmt lastStatement;
-    private @Nullable final MpqEditor mpqEditor;
-    private final Map<ObjectFileType, ObjectFile> dataStoreMap = Maps.newLinkedHashMap();
+    private @Nullable
+    final MpqEditor mpqEditor;
+    private final Map<ObjectFileType, ObjMod<? extends ObjMod.Obj>> dataStoreMap = Maps.newLinkedHashMap();
     private int id = 0;
-    private final Map<String, ObjectDefinition> objDefinitions = Maps.newLinkedHashMap();
+    private final Map<String, ObjMod.Obj> objDefinitions = Maps.newLinkedHashMap();
     private PrintStream outStream = System.err;
     private @Nullable WTS trigStrings = null;
     private final Optional<File> mapFile;
@@ -75,18 +82,20 @@ public class ProgramStateIO extends ProgramState {
         return res;
     }
 
-    ObjectFile getDataStore(String fileExtension) {
+    ObjMod<? extends ObjMod.Obj> getDataStore(String fileExtension) {
         return getDataStore(ObjectFileType.fromExt(fileExtension));
     }
 
-    private ObjectFile getDataStore(ObjectFileType filetype) throws Error {
-        ObjectFile dataStore = dataStoreMap.get(filetype);
+
+    private ObjMod<? extends ObjMod.Obj> getDataStore(ObjectFileType filetype) throws Error {
+        ObjMod<? extends ObjMod.Obj> dataStore = dataStoreMap.get(filetype);
         if (dataStore != null) {
             return dataStore;
         }
         if (mpqEditor == null) {
             // without a map: create empty object file
-            dataStore = new ObjectFile(filetype);
+            dataStore = filetypeToObjmod(filetype);
+            dataStore.setFormat(ObjMod.EncodingFormat.AUTO);
             dataStoreMap.put(filetype, dataStore);
             return dataStore;
         }
@@ -97,16 +106,44 @@ public class ProgramStateIO extends ProgramState {
             try {
                 if (mpqEditor.hasFile(fileName)) {
                     byte[] w3_ = mpqEditor.extractFile(fileName);
-                    dataStore = new ObjectFile(w3_, filetype);
-                    replaceTrigStrings(dataStore);
+                    try (Wc3BinInputStream in = new Wc3BinInputStream(new ByteArrayInputStream(w3_))) {
+                        switch (filetype) {
+                            case UNITS:
+                                dataStore = new W3U(in);
+                                break;
+                            case ITEMS:
+                                dataStore = new W3T(in);
+                                break;
+                            case DESTRUCTABLES:
+                                dataStore = new W3B(in);
+                                break;
+                            case DOODADS:
+                                dataStore = new W3D(in);
+                                break;
+                            case ABILITIES:
+                                dataStore = new W3A(in);
+                                break;
+                            case BUFFS:
+                                dataStore = new W3H(in);
+                                break;
+                            case UPGRADES:
+                                dataStore = new W3Q(in);
+                                break;
+                        }
+
+                        replaceTrigStrings(dataStore);
+                    }
+
                 } else {
-                    dataStore = new ObjectFile(filetype);
+                    dataStore = filetypeToObjmod(filetype);
+                    dataStore.setFormat(ObjMod.EncodingFormat.AUTO);
                 }
             } catch (IOException | InterruptedException e) {
                 // TODO maybe tell the user, that something has gone wrong
                 WLogger.info("Could not extract file: " + fileName);
                 WLogger.info(e);
-                dataStore = new ObjectFile(filetype);
+                dataStore = filetypeToObjmod(filetype);
+                dataStore.setFormat(ObjMod.EncodingFormat.AUTO);
             }
             dataStoreMap.put(filetype, dataStore);
 
@@ -120,21 +157,42 @@ public class ProgramStateIO extends ProgramState {
         return dataStore;
     }
 
-    private void replaceTrigStrings(ObjectFile dataStore) {
-        replaceTrigStringsInTable(dataStore.getOrigTable());
-        replaceTrigStringsInTable(dataStore.getModifiedTable());
+    @NotNull
+    private ObjMod<? extends ObjMod.Obj> filetypeToObjmod(ObjectFileType filetype) {
+        switch (filetype) {
+            case UNITS:
+                return new W3U();
+            case ITEMS:
+                return new W3T();
+            case DESTRUCTABLES:
+                return new W3B();
+            case DOODADS:
+                return new W3D();
+            case ABILITIES:
+                return new W3A();
+            case BUFFS:
+                return new W3H();
+            case UPGRADES:
+                return new W3Q();
+        }
+        return null;
     }
 
-    private void replaceTrigStringsInTable(ObjectTable modifiedTable) {
-        for (ObjectDefinition od : modifiedTable.getObjectDefinitions().values()) {
-            for (ObjectModification<?> mod : od.getModifications()) {
-                if (mod instanceof ObjectModificationString) {
-                    ObjectModificationString modS = (ObjectModificationString) mod;
-                    if (modS.getData().startsWith("TRIGSTR_")) {
+    private void replaceTrigStrings(ObjMod<? extends ObjMod.Obj> dataStore) {
+        replaceTrigStringsInTable(dataStore.getOrigObjs());
+        replaceTrigStringsInTable(dataStore.getCustomObjs());
+    }
+
+    private void replaceTrigStringsInTable(List<? extends ObjMod.Obj> modifiedTable) {
+        for (ObjMod.Obj od : modifiedTable) {
+            for (ObjMod.Obj.Mod mod : od.getMods()) {
+                if (mod.getValType() == ObjMod.ValType.STRING && mod.getVal() instanceof War3String) {
+                    War3String stringVal = (War3String) mod.getVal();
+                    if (stringVal.getVal().startsWith("TRIGSTR_")) {
                         try {
-                            int id = Integer.parseInt(modS.getData().substring("TRIGSTR_".length()), 10);
+                            int id = Integer.parseInt(stringVal.getVal().substring("TRIGSTR_".length()), 10);
                             String newVal = getTrigString(id);
-                            modS.setData(newVal);
+                            stringVal.set_val(newVal);
                         } catch (NumberFormatException e) {
                             // ignore
                         }
@@ -144,15 +202,15 @@ public class ProgramStateIO extends ProgramState {
         }
     }
 
-    private void deleteWurstObjects(ObjectFile unitStore) {
-        Iterator<Map.Entry<Integer, ObjectDefinition>> it = unitStore.getModifiedTable().getObjectDefinitions().entrySet().iterator();
-        while (it.hasNext()) {
-            ObjectDefinition od = it.next().getValue();
-            for (ObjectModification<?> om : od.getModifications()) {
-                if (om.getModificationId().equals("wurs") && om instanceof ObjectModificationInt) {
-                    ObjectModificationInt om2 = (ObjectModificationInt) om;
-                    if (om2.getData() == GENERATED_BY_WURST) {
-                        it.remove();
+    private void deleteWurstObjects(ObjMod<? extends ObjMod.Obj> unitStore) {
+        Iterator<? extends ObjMod.Obj> iterator = unitStore.getCustomObjs().iterator();
+        while (iterator.hasNext()) {
+            ObjMod.Obj next = iterator.next();
+            for (ObjMod.Obj.Mod om : next.getMods()) {
+                if (om.getId().getVal().equals("wurs") && om.getVal() instanceof War3Int) {
+                    War3Int val = (War3Int) om.getVal();
+                    if (val.getVal() == GENERATED_BY_WURST) {
+                        iterator.remove();
                         break;
                     }
                 }
@@ -161,14 +219,14 @@ public class ProgramStateIO extends ProgramState {
     }
 
 
-    String addObjectDefinition(ObjectDefinition objDef) {
+    String addObjectDefinition(ObjMod.Obj objDef) {
         id++;
         String key = "obj" + id;
         objDefinitions.put(key, objDef);
         return key;
     }
 
-    ObjectDefinition getObjectDefinition(String key) {
+    ObjMod.Obj getObjectDefinition(String key) {
         return objDefinitions.get(key);
     }
 
@@ -178,8 +236,8 @@ public class ProgramStateIO extends ProgramState {
 
         for (ObjectFileType fileType : ObjectFileType.values()) {
             WLogger.info("Writing back " + fileType);
-            ObjectFile dataStore = getDataStore(fileType);
-            if (!dataStore.isEmpty()) {
+            ObjMod<? extends ObjMod.Obj> dataStore = getDataStore(fileType);
+            if (!dataStore.getObjsList().isEmpty()) {
                 WLogger.info("Writing back filetype " + fileType);
                 writebackObjectFile(dataStore, fileType, inject);
             } else {
@@ -191,15 +249,15 @@ public class ProgramStateIO extends ProgramState {
 
     private void writeW3oFile() {
         Optional<File> objFile = getObjectEditingOutputFolder().map(fo -> new File(fo, "wurstCreatedObjects.w3o"));
-        try (BinaryDataOutputStream objFileStream = new BinaryDataOutputStream(objFile.get(), true)) {
-            objFileStream.writeInt(1); // version
+        try (Wc3BinOutputStream objFileStream = new Wc3BinOutputStream(objFile.get())) {
+            objFileStream.writeInt32(1); // version
             for (ObjectFileType fileType : ObjectFileType.values()) {
-                ObjectFile dataStore = getDataStore(fileType);
-                if (!dataStore.isEmpty()) {
-                    objFileStream.writeInt(1); // exists
-                    dataStore.writeTo(objFileStream);
+                ObjMod<? extends ObjMod.Obj> dataStore = getDataStore(fileType);
+                if (!dataStore.getObjs().isEmpty()) {
+                    objFileStream.writeInt32(1); // exists
+                    dataStore.write(objFileStream, ObjMod.EncodingFormat.AS_DEFINED);
                 } else {
-                    objFileStream.writeInt(0); // does not exist
+                    objFileStream.writeInt32(0); // does not exist
                 }
             }
         } catch (Error | IOException e) {
@@ -207,15 +265,20 @@ public class ProgramStateIO extends ProgramState {
         }
     }
 
-    private void writebackObjectFile(ObjectFile dataStore, ObjectFileType fileType, boolean inject) throws Error {
-        try {
+    private void writebackObjectFile(ObjMod<? extends ObjMod.Obj> dataStore, ObjectFileType fileType, boolean inject) throws Error {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try (Wc3BinOutputStream out = new Wc3BinOutputStream(baos)) {
             Optional<File> folder = getObjectEditingOutputFolder();
 
-            byte[] w3u = dataStore.writeToByteArray();
+            dataStore.write(out, ObjMod.EncodingFormat.AS_DEFINED);
 
-            // wurst exported objects
+            out.close();
+
+            byte[] w3_ = baos.toByteArray();
+
+            // TODO  wurst exported objects
             FileUtils.write(
-                dataStore.exportToWurst(fileType),
+                exportToWurst(dataStore, fileType),
                 new File(folder.get(), "WurstExportedObjects_" + fileType.getExt() + ".wurst.txt"));
 
             if (inject) {
@@ -224,7 +287,7 @@ public class ProgramStateIO extends ProgramState {
                 }
                 String filenameInMpq = "war3map." + fileType.getExt();
                 mpqEditor.deleteFile(filenameInMpq);
-                mpqEditor.insertFile(filenameInMpq, w3u);
+                mpqEditor.insertFile(filenameInMpq, w3_);
             }
         } catch (Exception e) {
             WLogger.severe(e);
@@ -232,6 +295,72 @@ public class ProgramStateIO extends ProgramState {
         }
 
     }
+
+    public String exportToWurst(ObjMod<? extends ObjMod.Obj> dataStore, ObjectFileType fileType) throws IOException {
+
+        Appendable out = new StringBuilder();
+        out.append("package WurstExportedObjects_").append(fileType.getExt()).append("\n");
+        out.append("import ObjEditingNatives\n\n");
+
+        out.append("// Modified Table (contains all custom objects)\n\n");
+        exportToWurst(dataStore.getCustomObjs(), fileType, out);
+
+        out.append("// Original Table (contains all modified default objects)\n" +
+            "// Wurst does not support modifying default objects\n" +
+            "// but you can copy these functions and replace 'xxxx' with a new, custom id.\n\n");
+        exportToWurst(dataStore.getOrigObjs(), fileType, out);
+
+        return out.toString();
+    }
+
+    public void exportToWurst(List<? extends ObjMod.Obj> customObjs, ObjectFileType fileType, Appendable out) throws IOException {
+        for (ObjMod.Obj obj : customObjs) {
+            String oldId = obj.getBaseId().getVal();
+            String newId = (obj.getNewId() != null ? obj.getNewId().getVal() : "xxxx");
+            out.append("@compiletime function create_").append(fileType.getExt()).append("_").append(newId)
+                .append("()\n");
+            out.append("	let def = createObjectDefinition(\"").append(fileType.getExt()).append("\", '");
+            out.append(newId);
+            out.append("', '");
+            out.append(oldId);
+            out.append("')\n");
+            for (ObjMod.Obj.Mod m : obj.getMods()) {
+                if (fileType.usesLevels() && m instanceof ObjMod.Obj.ExtendedMod) {
+                    ObjMod.Obj.ExtendedMod extendedMod = (ObjMod.Obj.ExtendedMod) m;
+                    out.append("\t..setLvlData").append(valTypeToFuncPostfix(extendedMod.getValType())).append("(\"");
+                    out.append(m.toString());
+                    out.append("\", ")
+                        .append(String.valueOf(extendedMod.getLevel()))
+                        .append(", ")
+                        .append(String.valueOf(extendedMod.getDataPt())).append(", ")
+                        .append((extendedMod.getValType() == ObjMod.ValType.STRING) ?
+                            Utils.escapeString(extendedMod.getVal().toString()) :
+                            extendedMod.getVal().toString())
+                        .append(")\n");
+                } else {
+                    out.append("\t..set").append(valTypeToFuncPostfix(m.getValType())).append("(\"");
+                    out.append(m.toString());
+                    out.append("\", ").append(m.getVal().toString()).append(")\n");
+                }
+            }
+            out.append("\n\n");
+        }
+    }
+
+    private String valTypeToFuncPostfix(ObjMod.ValType valType) {
+        switch (valType) {
+            case INT:
+                return "Int";
+            case REAL:
+                return "Real";
+            case UNREAL:
+                return "Unreal";
+            case STRING:
+                return "String";
+        }
+        return "Int";
+    }
+
 
     private Optional<File> getObjectEditingOutputFolder() {
         if (!mapFile.isPresent()) {
