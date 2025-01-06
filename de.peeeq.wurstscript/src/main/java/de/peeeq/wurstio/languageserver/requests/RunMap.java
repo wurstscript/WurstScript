c44c70133b9f22844f307e2172966292b79bce4c
Frotty <Frotty@users.noreply.github.com>
2025-01-06 23:20:24 +0100

    Fix exception when hot reloading

--- de.peeeq.wurstscript/src/main/java/de/peeeq/wurstio/languageserver/requests/RunMap.java
+++ de.peeeq.wurstscript/src/main/java/de/peeeq/wurstio/languageserver/requests/RunMap.java
@@ -98,8 +98,10 @@ public class RunMap extends MapRequest {
 
         // first we copy in same location to ensure validity
         File buildDir = getBuildDir();
-        mapLastModified = map.get().lastModified();
-        mapPath = map.get().getAbsolutePath();
+        if (map.isPresent()) {
+            mapLastModified = map.get().lastModified();
+            mapPath = map.get().getAbsolutePath();
+        }
         Optional<File> testMap = map.map($ -> new File(buildDir, "WurstRunMap.w3x"));
         CompilationResult result = compileScript(modelManager, gui, testMap, projectConfig, buildDir, false);
 
